28c4d476eba307b16ec7cb6df310559a46c4795e
Shradha <sjoshi@ex2india.com>
2018-09-24 23:34:19 +0530

    Data between Components

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, Output, EventEmitter} from '@angular/core'
+
+@Component({
+    selector: 'event-thumbnail',
+    template: `
+    <div class="well hoverwell thumbnail">
+        <h2>{{event.name}}</h2>
+        <div>Date:{{event.date}}</div>
+        <div>Time:{{event.time}}</div>
+        <div>Price:\${{event.price}}</div>
+        <div>Location:{{event.location.address + " " + event.location.city  + ", " +  event.location.country}}</div>
+        <button class="btn btn-primary" (click)="handleClickMe()">Click Me!</button>
+    </div>
+    `
+})
+
+export class EventThumbnailComponent
+{
+    @Input() event:any
+    @Output() eventClick = new EventEmitter()
+
+    handleClickMe()
+    {
+        this.eventClick.emit(this.event.name)
+    }
+}
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,10 +2,16 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'events-list',
-    templateUrl: './events-list.component.html'
+    template:`
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr>
+        <event-thumbnail [event]="event1" (eventClick)="hadleEventClicked($event)"></event-thumbnail>
+    </div>
+    `
 })
 export class EventListComponent{
-    event = {
+    event1 = {
         id:1,
         name: 'Angular Connect',
         date: '9/23/2018',
@@ -18,5 +24,10 @@ export class EventListComponent{
             city:'London',
             country:'England    '
         }
+    }
+
+    hadleEventClicked(data)
+    {
+        console.log("Received: " + data)
     }
 }
