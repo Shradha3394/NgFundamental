6e949023d59c58deff5dbf11230624135d8231bd
Shradha <sjoshi@ex2india.com>
2018-09-27 23:41:13 +0530

    Service Demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventListComponent } from './events/events-list.component';
 import {EventThumbnailComponent} from './events/event-thumbnail.component'
 import {NavBarComponent} from './nav/navbar.component'
+import {EventService} from './events/shared/event.service'
 
 @NgModule({
   imports: [
@@ -13,6 +14,7 @@ import {NavBarComponent} from './nav/navbar.component'
   declarations: [
     EventsAppComponent, EventListComponent, EventThumbnailComponent, NavBarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
